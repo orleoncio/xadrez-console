d658841e23427443049421c0ae7614d72a80e768
orleoncio <orleonciofilho@hotmail.com>
2025-02-27 16:30:57 -0300

    Criação da classe posição

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -6,7 +7,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            Posicao p = new Posicao(3, 4);
 
+            Console.WriteLine("Posição: " + p);
         }
     }
 }
